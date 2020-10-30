1f945a845a87ed70ef009344ebdb3ad77bbedc13
Garedbang <djhsid14@gmail.com>
2020-10-30 06:57:02 +0200

    section 3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
+import AskUserName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
+console.log('May I have your name?');
+const userResponse = await AskUserName();
+console.log(`Hello, ${userResponse}!`);
